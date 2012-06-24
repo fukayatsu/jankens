5c27410e574056115a75a134f8716a6b2e304f26
fukayatsu <fukayatsu@gmail.com>
2012-06-24 11:29:53 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jankens::Application.config.secret_token = '5e60c8d0af35c26026838a5fd738bb2f936615cefa5316a201255aa17fc7797307f77c11694fa1fb7b4f7cc06ba54e82bed7486157e6a3bccb23c48c6f182e84'
