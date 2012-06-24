9ecb27289b162c2ddca27363dee78548dee4a613
fukayatsu <fukayatsu@gmail.com>
2012-06-24 11:40:48 +0900

    modify config for heroku

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ end
 
 module Jankens
   class Application < Rails::Application
+
+    # For Heroku
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
